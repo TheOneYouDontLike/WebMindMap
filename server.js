300055ad2197cc299fe078b7aebeaa35c9a3b882
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-04-08 23:57:30 +0200

    Pocket spaghetti is working.

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var express = require('express'),
     request = require('request');
 
 var app = express(),
-    port = 1234;
+    port = 1234,
+    requestToken = '',
+    consumerKey = "39832-01713ea2fce3d5c62db85677";
 
 app.get('/', function(req, res) {
-
-    var consumerKey = "39832-01713ea2fce3d5c62db85677";
 
     var options = {
         headers: {
@@ -19,21 +19,54 @@ app.get('/', function(req, res) {
         body: 'consumer_key=' + consumerKey + '&redirect_uri=http://localhost/'
     };
 
-    var requestToken = '';
+    // get REQUEST TOKEN
     request.post(options, function(error, response, body) {
         requestToken = JSON.parse(body).code;
-        console.log(requestToken);
+        console.log('request token: ' + requestToken);
 
         var redirectUrl = 'https://getpocket.com/auth/authorize?request_token=' + requestToken + '&redirect_uri=http://localhost:1234/working';
         res.redirect(redirectUrl);
     });
-
-    //res.send('working');
-
 });
 
 app.get('/working', function(req, res) {
-    res.send('authorized');
+    // get ACCESS TOKEN
+    var accessTokenRequest = {
+        headers: {
+            "content-type": "application/json",
+            "X-Accept": "application/json"
+        },
+        url: 'https://getpocket.com/v3/oauth/authorize',
+        body: JSON.stringify(
+            {
+                "consumer_key": consumerKey,
+                "code": requestToken
+            })
+    };
+
+    request.post(accessTokenRequest, function(error, response, body) {
+        var accessToken = JSON.parse(body).access_token;
+        console.log(accessToken);
+
+        // get ALL LINKS
+        var getDevelopmentLinks = {
+            headers: {
+                "content-type": "application/json",
+                "X-Accept": "application/json"
+            },
+            url: 'https://getpocket.com/v3/get',
+            body: JSON.stringify(
+                {
+                    "consumer_key": consumerKey,
+                    "access_token": accessToken,
+                    "tag": "development"
+                })
+        };
+
+        request.post(getDevelopmentLinks, function(error, response, body) {
+            res.send(body);
+        });
+    });
 });
 
 app.listen(port, function() {
