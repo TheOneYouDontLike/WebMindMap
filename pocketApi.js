a499380d78569af86801b0733d6393f11b390602
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-04-10 23:07:44 +0200

    Extracted pocket api to module.

---
+++ pocketApi.js
@@ -0,0 +1,87 @@
+'use strict';
+
+var request = require('request'),
+    pocketApiConsumerKey = require('./consumerKey.js');
+
+var pocketApiConstants = {
+    headers: {
+        'content-type': 'application/json',
+        'X-Accept': 'application/json'
+    },
+    urls: {
+        requestToken: 'https://getpocket.com/v3/oauth/request',
+        accessToken: 'https://getpocket.com/v3/oauth/authorize',
+        get: 'https://getpocket.com/v3/get'
+    },
+    consumerKey: pocketApiConsumerKey
+};
+
+var pocketApi = function() {
+    var module = {};
+
+    module.getRequestToken = getRequestToken;
+    module.getAccessToken = getAccessToken;
+    module.getAll = getAll;
+
+    function getRequestToken(callback) {
+        var options = {
+            headers: pocketApiConstants.headers,
+            url: pocketApiConstants.urls.requestToken,
+            body: JSON.stringify({
+                'consumer_key': pocketApiConstants.consumerKey,
+                'redirect_uri': "http://localhost:1234"
+            })
+        };
+
+        request.post(options, function(error, response, body) {
+            if (error) { callback(error, null); }
+
+            var requestToken = JSON.parse(body).code;
+            console.log('request token: ' + requestToken);
+
+            callback(null, requestToken);
+        });
+    }
+
+    function getAccessToken(requestToken, callback) {
+        var accessTokenRequest = {
+            headers: pocketApiConstants.headers,
+            url: pocketApiConstants.urls.accessToken,
+            body: JSON.stringify({
+                "consumer_key": pocketApiConstants.consumerKey,
+                "code": requestToken
+            })
+        };
+
+        request.post(accessTokenRequest, function(error, response, body) {
+            if (error) { callback(error, null); }
+
+            var accessToken = JSON.parse(body).access_token;
+            console.log('access token: ' + accessToken);
+
+            callback(null, accessToken);
+        });
+    }
+
+    function getAll(accessToken, callback) {
+        var getDevelopmentLinks = {
+            headers: pocketApiConstants.headers,
+            url: pocketApiConstants.urls.get,
+            body: JSON.stringify({
+                "consumer_key": pocketApiConstants.consumerKey,
+                "access_token": accessToken,
+                "tag": "development"
+            })
+        };
+
+        request.post(getDevelopmentLinks, function(error, response, body) {
+            if (error) { callback(error, null); }
+
+            callback(null, body);
+        });
+    }
+
+    return module;
+};
+
+module.exports = pocketApi();
--- server.js
+++ server.js
@@ -1,75 +1,27 @@
 'use strict';
 
 var express = require('express'),
-    request = require('request');
+    pocketApi = require('./pocketApi.js');
 
 var app = express(),
     port = 1234,
     requestToken = '';
 
-var pocketApi = {
-    headers: {
-        'content-type': 'application/json',
-        'X-Accept': 'application/json'
-    },
-    urls: {
-        requestToken: 'https://getpocket.com/v3/oauth/request',
-        accessToken: 'https://getpocket.com/v3/oauth/authorize',
-        get: 'https://getpocket.com/v3/get'
-    },
-    consumerKey: '39832-01713ea2fce3d5c62db85677'
-};
-
 app.get('/', function(req, res) {
-    var options = {
-        headers: pocketApi.headers,
-        url: pocketApi.urls.requestToken,
-        body: JSON.stringify({
-            'consumer_key': pocketApi.consumerKey,
-            'redirect_uri': "http://localhost:1234"
-        })
-    };
-
-    // get REQUEST TOKEN
-    request.post(options, function(error, response, body) {
-        requestToken = JSON.parse(body).code;
-        console.log('request token: ' + requestToken);
-
-        var redirectUrl = 'https://getpocket.com/auth/authorize?request_token=' + requestToken + '&redirect_uri=http://localhost:1234/working';
+    pocketApi.getRequestToken(function(error, obtainedRequestToken) {
+        requestToken = obtainedRequestToken;
+        var redirectUrl = 'https://getpocket.com/auth/authorize?request_token=' + obtainedRequestToken + '&redirect_uri=http://localhost:1234/working';
         res.redirect(redirectUrl);
     });
 });
 
 app.get('/working', function(req, res) {
-    // get ACCESS TOKEN
-    var accessTokenRequest = {
-        headers: pocketApi.headers,
-        url: pocketApi.urls.accessToken,
-        body: JSON.stringify({
-            "consumer_key": pocketApi.consumerKey,
-            "code": requestToken
-        })
-    };
-
-    request.post(accessTokenRequest, function(error, response, body) {
-        var accessToken = JSON.parse(body).access_token;
-        console.log('access token: ' + accessToken);
-
-        // get ALL LINKS
-        var getDevelopmentLinks = {
-            headers: pocketApi.headers,
-            url: pocketApi.urls.get,
-            body: JSON.stringify({
-                "consumer_key": pocketApi.consumerKey,
-                "access_token": accessToken,
-                "tag": "development"
-            })
-        };
-
-        request.post(getDevelopmentLinks, function(error, response, body) {
-            res.send(body);
+    pocketApi.getAccessToken(requestToken, function(error, accessToken) {
+        pocketApi.getAll(accessToken, function(error, data) {
+            res.send(data);
         });
     });
+
 });
 
 app.listen(port, function() {
