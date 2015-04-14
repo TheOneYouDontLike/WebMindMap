1ce20bb30edd9b5e89facf5edd13f2ebc0d9dbba
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-04-14 23:42:43 +0200

    Saving pocket data to state and displaying on dummy list.

--- components/app.jsx
+++ components/app.jsx
@@ -1,12 +1,13 @@
 'use strict';
 
 var React = require('react'),
-    superagent = require('superagent');
+    superagent = require('superagent'),
+    _ = require('lodash');
 
 var mainContainer = document.getElementById('main-container');
 
 var HomePage = React.createClass({
-    connectWithPocket: function() {
+    _connectWithPocket: function() {
         if (window.localStorage.ACCESS_TOKEN) {
             alert('Already connected!');
         }
@@ -26,6 +27,12 @@ var HomePage = React.createClass({
             });
     },
 
+    getInitialState: function() {
+        return {
+            pocketData: {}
+        };
+    },
+
     componentDidMount: function() {
         if (!window.localStorage.ACCESS_TOKEN) {
             superagent
@@ -42,17 +49,29 @@ var HomePage = React.createClass({
             superagent
                 .get('/getArticles/' + window.localStorage.ACCESS_TOKEN)
                 .end(function(error, response) {
-                    console.log(response.body);
-                });
+                    var sortedList = _.toArray(response.body.list);
+                    this.setState({ pocketData: sortedList });
+                }.bind(this));
         }
     },
 
     render: function() {
+        var articlesToRender = _.map(this.state.pocketData, function(article) {
+            return (
+                <div className="row">
+                    <div className="col s2">{ article.item_id }</div>
+                    <div className="col s4">{ article.given_title }</div>
+                    <div className="col s4">{ article.given_url }</div>
+                </div>
+            );
+        });
+
         return (
             <div className="HomePage">
                 <h1>Web Mind Map</h1>
                 <p>Connect with your pocket app</p>
-                <button type="button" onClick={this.connectWithPocket}>Connect</button>
+                <button type="button" onClick={this._connectWithPocket}>Connect</button>
+                { articlesToRender }
             </div>
         );
     }
